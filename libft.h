3e891ab109b9b22715d765c886c991e6ead619b2
Felipe <Feemoyo@lipemoreira.yorgos@gmail.com>
2021-12-10 22:54:42 +0100

    add strcmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fmoreira <fmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 19:55:37 by fmoreira          #+#    #+#             */
-/*   Updated: 2021/06/24 14:18:07 by fmoreira         ###   ########.fr       */
+/*   Updated: 2021/12/10 22:53:42 by fmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_atoi(const char *nptr);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
