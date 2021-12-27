5a6410c76e5bc5133de2bf71aa5995e6645276e4
Feemoyo <lipemoreira.yorgos@gmail.com>
2021-12-27 18:53:41 +0100

    strcmp rework

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: fmoreira <fmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 22:48:42 by fmoreira          #+#    #+#             */
-/*   Updated: 2021/12/10 22:58:08 by fmoreira         ###   ########.fr       */
+/*   Updated: 2021/12/27 18:51:47 by fmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int	ft_strcmp(const char *s1, const char *s2)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while (str1[i] || str2[i])
+	while (str1[i] != 0 || str2[i] != 0)
 	{
 		if (str1[i] != str2[i])
-			return (str1[i] - str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (0);
